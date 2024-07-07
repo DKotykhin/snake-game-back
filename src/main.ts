1299029126a833a1c63ed4bda443830d45843072
Dmytro Kotykhin <kotykhin_d@ukr.net>
2024-07-07 20:24:14 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 const logger = new Logger('main.ts');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT || 4000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
